2f96cbe404e9e4bdc07a876697656c2871d1121a
jojoyal <josepjoyal@gmail.com>
2023-03-01 20:18:13 +0530

    fixed banner height

--- src/App.js
+++ src/App.js
@@ -4,20 +4,23 @@ import './App.css'
 import NavBar from '../src/Components/NavBar/NavBar'
 import Banner from './Components/Banner/Banner';
 import RowPost from './Components/RowPost/RowPost';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { ActionMovies, ComedyMovies, Documentaries, HorrorMovies, Netflix_Originals, RomanceMovies } from './urls';
 
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
-      <Banner/>
-      <RowPost title={'Netflix Originals'} url={Netflix_Originals}/>
-      <RowPost title={'Action'} isSmall={true} url={ActionMovies}/>
-      <RowPost title={'Comedy'} isSmall={true} url={ComedyMovies}/>
-      <RowPost title={'Horror'} isSmall={true} url={HorrorMovies}/>
-      <RowPost title={'Romance'} isSmall={true} url={RomanceMovies}/>
-      <RowPost title={'Documentaries'} isSmall={true} url={Documentaries}/>
+      <Router>
+        <NavBar/>
+        <Banner/>
+        <RowPost title={'Netflix Originals'} url={Netflix_Originals}/>
+        <RowPost title={'Action'} isSmall={true} url={ActionMovies}/>
+        <RowPost title={'Comedy'} isSmall={true} url={ComedyMovies}/>
+        <RowPost title={'Horror'} isSmall={true} url={HorrorMovies}/>
+        <RowPost title={'Romance'} isSmall={true} url={RomanceMovies}/>
+        <RowPost title={'Documentaries'} isSmall={true} url={Documentaries}/>
+      </Router>
     </div>
   );
 }
